6b9a962447a65040686256174fbfd570de243562
damyj <3206279058@qq.com>
2020-02-20 23:18:52 +0800

    -

--- sliding-root-nav/src/main/kotlin/com/yarolegovich/slidingrootnav/util/SideNavUtils.kt
+++
@@ -1,5 +0,0 @@
-package com.yarolegovich.slidingrootnav.util
-
-fun slideEvaluate(fraction: Float, startValue: Float, endValue: Float): Float {
-  return startValue + fraction * (endValue - startValue)
-}
